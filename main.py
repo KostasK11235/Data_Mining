68680ea3a7a33f8cdc91a01bd87aa9b67d2a16d5
kostas <kapolasnikos@gmail.com>
2023-05-14 21:06:06 +0300

    done most of the work for part 1

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 # read data file
 df = pd.read_csv('data.csv')
 
+# get the number of null values in each column
+print("Number of null values in each column: ")
+print(df.isnull().sum())
+
+# Describe values for each column: number of values, min, max, mean, standard deviation, Q1, Q2 and Q3 quartiles
+print("Describe: \n", df.describe().round(3).to_string())
+
+# boxplot diagrams for every column except: Entity, Continent, Latitude, Longitude and Date
+for column in df.drop(['Entity', 'Continent', 'Latitude', 'Longitude', 'Date'], axis=1):
+    plt.figure()
+    df.boxplot([column])
+    plt.show()
+
+# Heatmap diagram
+df_numeric = df.drop(['Entity', 'Continent', 'Date'], axis=1)
+matrix = df_numeric.corr().round(3)
+sns.heatmap(matrix, annot=True)
+plt.show()
 
 # GDP/Capita vs Hospital beds per 1000 people
 gdp_beds = df.groupby('GDP/Capita')['Hospital beds per 1000 people'].mean()
@@ -82,3 +100,29 @@ plt.title('Number of tests done each date')
 plt.xlabel('Date')
 plt.ylabel('Number of Daily tests')
 plt.show()
+
+# We set the criteria for a country to have poor data, we find those countries and remove them from the dataframe
+rows_per_country = df.groupby('Entity').size()
+print(rows_per_country.values)
+
+nulls_per_country = df.groupby('Entity').apply(lambda x: x.isnull().sum())
+print(nulls_per_country)
+
+# store and print the countries where missing data from columns 'Daily tests','Cases','Deaths' are more than half country's data
+bad_data = 0
+countries_to_remove = []
+for i in range(0, 104):
+    for j in range(12, 15):
+        # print("->", rows_per_country.values[i] - nulls_per_country.values[i][j])
+        if (rows_per_country.values[i] - nulls_per_country.values[i][j]) < rows_per_country.values[i]/2:
+            bad_data = 1
+    if bad_data:
+        countries_to_remove.append(nulls_per_country.index[i])
+        print("Country name: ", nulls_per_country.index[i], ",Total values: ", rows_per_country.values[i],
+              ",Nulls: Daily tests: ", nulls_per_country.values[i][12], ",Cases: ", nulls_per_country.values[i][13],
+              ",Deaths: ", nulls_per_country.values[i][14])
+        bad_data = 0
+
+# remove from dataframe all countries found in the previous step
+for country_name in countries_to_remove:
+    df = df.drop(df.loc[df['Entity'] == country_name].index)
