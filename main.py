f131c7555d8542551bd08db0d9b3fe42df86a311
kostas <kapolasnikos@gmail.com>
2023-05-24 18:46:02 +0300

    erased not shared dates

--- main.py
+++ main.py
@@ -9,97 +9,27 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 # read data file
 df = pd.read_csv('data.csv')
 
+df['Data'] = pd.to_datetime(df['Date'])
+dates = df.groupby('Date').size()
+unique_countries = df['Entity'].unique()
+
+outdates = []
+for i in range(0, len(dates)):
+    if dates.values[i] < len(unique_countries):
+        outdates.append(dates.index[i])
+
+print(outdates)
+mask = df['Date'].isin(outdates)
+df_filtered = df[~mask]
+
+print(df_filtered.info())
+
 # get the number of null values in each column
 print("Number of null values in each column: ")
 print(df.isnull().sum())
 
 # Describe values for each column: number of values, min, max, mean, standard deviation, Q1, Q2 and Q3 quartiles
 print("Describe: \n", df.describe().round(3).to_string())
-
-# boxplot diagrams for every column except: Entity, Continent, Latitude, Longitude and Date
-for column in df.drop(['Entity', 'Continent', 'Latitude', 'Longitude', 'Date'], axis=1):
-    plt.figure()
-    df.boxplot([column])
-    plt.show()
-
-# Heatmap diagram
-df_numeric = df.drop(['Entity', 'Continent', 'Date'], axis=1)
-matrix = df_numeric.corr().round(3)
-sns.heatmap(matrix, annot=True)
-plt.show()
-
-# GDP/Capita vs Hospital beds per 1000 people
-gdp_beds = df.groupby('GDP/Capita')['Hospital beds per 1000 people'].mean()
-gdp_beds.plot(kind='bar')
-plt.title('GDP/Capita vs Hospital beds per 1000 people')
-plt.xlabel('GDP/Capital')
-plt.ylabel('Hospital beds per 1000 people')
-plt.show()
-
-# GDP/capita vs Medical doctors per 1000 people
-gdp_doctors = df.groupby('GDP/Capita')['Medical doctors per 1000 people'].mean()
-gdp_doctors.plot(kind='bar')
-plt.title('GDP/capita vs Medical doctors per 1000 people')
-plt.xlabel('GDP/Capita')
-plt.ylabel('Medical doctors per 1000 people')
-plt.show()
-
-# Hospital beds per 1000 people vs Deaths
-beds = df.groupby('Hospital beds per 1000 people')['Deaths'].sum()
-beds.plot(kind='bar')
-plt.title('Hospital beds per 1000 people vs Deaths')
-plt.xlabel('Hospital beds per 1000 people')
-plt.ylabel('Deaths')
-plt.show()
-
-# Medical doctors per 1000 people vs Deaths
-md_deaths = df.groupby('Medical doctors per 1000 people')['Deaths'].sum()
-md_deaths.plot(kind='bar')
-plt.title('Medical doctors per 1000 people vs Deaths')
-plt.xlabel('Medical Doctors per 1000 people')
-plt.ylabel('Deaths')
-plt.show()
-
-# Population vs Daily tests
-population_dailyTests = df.groupby('Population')['Daily tests'].sum()
-population_dailyTests.plot(kind='bar')
-plt.title('Population vs Daily tests')
-plt.xlabel('Population')
-plt.ylabel('Total Number of Daily tests')
-plt.show()
-
-# Populations vs Cases
-population_cases = df.groupby('Population')['Cases'].sum()
-population_cases.plot(kind='bar')
-plt.title('Populations vs Cases')
-plt.xlabel('Population')
-plt.ylabel('Total number of Cases')
-plt.show()
-
-# Population vs Deaths
-population_deaths = df.groupby('Population')['Deaths'].sum()
-population_deaths.plot(kind='bar')
-plt.title('Population vs Deaths')
-plt.xlabel('Population')
-plt.ylabel('Deaths')
-plt.show()
-
-# Median age vs Deaths
-medianAge_deaths = df.groupby('Median age')['Deaths'].sum()
-medianAge_deaths.plot(kind='bar')
-plt.title('Median age vs Deaths')
-plt.xlabel('Median age')
-plt.ylabel('Deaths')
-plt.show()
-
-# Number of tests done each date
-df['Date'] = pd.to_datetime(df['Date'])
-tests_by_date = df.groupby('Date')['Daily tests'].sum()
-tests_by_date.plot(kind='bar')
-plt.title('Number of tests done each date')
-plt.xlabel('Date')
-plt.ylabel('Number of Daily tests')
-plt.show()
 
 # We set the criteria for a country to have poor data, we find those countries and remove them from the dataframe
 rows_per_country = df.groupby('Entity').size()
@@ -126,3 +56,6 @@ for i in range(0, 104):
 # remove from dataframe all countries found in the previous step
 for country_name in countries_to_remove:
     df = df.drop(df.loc[df['Entity'] == country_name].index)
+
+print("Number of null values in each column: ")
+print(df.isnull().sum())
