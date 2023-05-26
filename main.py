30d76a7cfa23eaf366dca22a7ca7617441621c0f
kostas <kapolasnikos@gmail.com>
2023-05-26 12:49:00 +0300

    done with code for first part

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from scipy.stats import zscore
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
+
 # read data file
 df = pd.read_csv('data.csv')
 
-df['Data'] = pd.to_datetime(df['Date'])
+# get the non common dates and remove them from the dataframe
+df['Date'] = pd.to_datetime(df['Date'])
 dates = df.groupby('Date').size()
 unique_countries = df['Entity'].unique()
 
@@ -26,16 +28,16 @@ print(df_filtered.info())
 
 # get the number of null values in each column
 print("Number of null values in each column: ")
-print(df.isnull().sum())
+print(df_filtered.isnull().sum())
 
 # Describe values for each column: number of values, min, max, mean, standard deviation, Q1, Q2 and Q3 quartiles
-print("Describe: \n", df.describe().round(3).to_string())
+print("Describe: \n", df_filtered.describe().round(3).to_string())
 
 # We set the criteria for a country to have poor data, we find those countries and remove them from the dataframe
-rows_per_country = df.groupby('Entity').size()
+rows_per_country = df_filtered.groupby('Entity').size()
 print(rows_per_country.values)
 
-nulls_per_country = df.groupby('Entity').apply(lambda x: x.isnull().sum())
+nulls_per_country = df_filtered.groupby('Entity').apply(lambda x: x.isnull().sum())
 print(nulls_per_country)
 
 # store and print the countries where missing data from columns 'Daily tests','Cases','Deaths' are more than half country's data
@@ -55,7 +57,24 @@ for i in range(0, 104):
 
 # remove from dataframe all countries found in the previous step
 for country_name in countries_to_remove:
-    df = df.drop(df.loc[df['Entity'] == country_name].index)
+    df_filtered = df_filtered.drop(df_filtered.loc[df['Entity'] == country_name].index)
 
 print("Number of null values in each column: ")
-print(df.isnull().sum())
+print(df_filtered.isnull().sum())
+
+# df_filtered.to_csv('clean_data.csv', index=False)
+
+nulls_per_country = df_filtered.groupby('Entity').apply(lambda x: x.isnull().sum())
+print(nulls_per_country)
+
+groups = df_filtered.groupby('Entity')
+
+df_filtered[['Daily tests', 'Cases', 'Deaths']] = df_filtered[['Daily tests', 'Cases', 'Deaths']].interpolate(method='linear', limit_direction='forward')
+df_filtered[['Daily tests', 'Cases', 'Deaths']] = df_filtered[['Daily tests', 'Cases', 'Deaths']].round()
+
+# df_filtered['Cases'] = df_filtered['Cases'].interpolate(method='')
+df_filtered.to_csv('interpolated_data.csv', index=False)
+
+print("Number of null values in each column: ")
+print(df_filtered.isnull().sum())
+
