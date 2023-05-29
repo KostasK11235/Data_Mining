6e9b63bf468546d05d5d5e4f13651fd2f1406f1a
kostas <kapolasnikos@gmail.com>
2023-05-29 20:43:20 +0300

    done with clustering

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -10,10 +13,7 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 # read data file
 df = pd.read_csv('data.csv')
 
-<<<<<<< HEAD
 # get the non common dates and remove them from the dataframe
-=======
->>>>>>> efe47bc16a0bcbf1d6ba9a08c968af2917280409
 df['Date'] = pd.to_datetime(df['Date'])
 dates = df.groupby('Date').size()
 unique_countries = df['Entity'].unique()
@@ -63,7 +63,6 @@ for country_name in countries_to_remove:
     df_filtered = df_filtered.drop(df_filtered.loc[df['Entity'] == country_name].index)
 
 print("Number of null values in each column: ")
-<<<<<<< HEAD
 print(df_filtered.isnull().sum())
 
 # df_filtered.to_csv('clean_data.csv', index=False)
@@ -77,11 +76,41 @@ df_filtered[['Daily tests', 'Cases', 'Deaths']] = df_filtered[['Daily tests', 'C
 df_filtered[['Daily tests', 'Cases', 'Deaths']] = df_filtered[['Daily tests', 'Cases', 'Deaths']].round()
 
 # df_filtered['Cases'] = df_filtered['Cases'].interpolate(method='')
-df_filtered.to_csv('interpolated_data.csv', index=False)
+# df_filtered.to_csv('interpolated_data.csv', index=False)
 
-print("Number of null values in each column: ")
-print(df_filtered.isnull().sum())
+grouped_data = df_filtered.groupby('Entity')
+country_Hospital_beds_per1000 = grouped_data['Hospital beds per 1000 people'].first()
+country_doctors_per1000 = grouped_data['Medical doctors per 1000 people'].first()
+country_population = grouped_data['Population'].first()
+country_total_daily_tests = grouped_data['Daily tests'].sum()
+country_total_cases = grouped_data['Cases'].max()
+country_total_deaths = grouped_data['Deaths'].max()
 
-=======
-print(df.isnull().sum())
->>>>>>> efe47bc16a0bcbf1d6ba9a08c968af2917280409
+country_Hospital_beds = country_Hospital_beds_per1000*(country_population//1000)
+country_doctors = country_doctors_per1000*(country_population//1000)
+
+positivity_rate = country_total_cases/country_total_daily_tests
+death_rate = country_total_deaths/country_total_cases
+cases_population = country_total_cases/country_population
+beds_cases = country_Hospital_beds/country_total_cases
+doctors_cases = country_doctors/country_total_cases
+
+# get all percentages in a single Dataframe
+concatenated_percentages = pd.concat([positivity_rate, death_rate, cases_population, beds_cases, doctors_cases], axis=1)
+
+# start clustering and evaluate number of clusters
+max_k = 10
+best_score = -1
+best_k = -1
+
+for k in range(2, max_k+1):
+    k_means = KMeans(n_clusters=k, n_init=10)
+    labels = k_means.fit_predict(concatenated_percentages)
+    score = silhouette_score(concatenated_percentages, labels)
+    print("#k: ", k, "score: ", score)
+    if score > best_score:
+        best_score = score
+        best_k = k
+
+print("optimal k: ", best_k)
+print("Silhouette score: ", best_score)
